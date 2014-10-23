80c95b44d969ab5962a4dae06ac620c16abfd8da
TL-kaiya <917401186@qq.com>
2014-10-23 16:05:15 +0800

    ww

---
+++ src/argumentfinal/FinalClass.java
@@ -0,0 +1,44 @@
+/**
+ *	2014年10月23日 下午2:32:33
+ *	TangLi
+ */
+package argumentfinal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TangLi
+ * 2014年10月23日下午2:32:33
+ */
+public final class FinalClass
+{
+	
+	public String name ;
+	
+	public FinalClass(String value)
+	{
+		this.name = value;
+	}
+
+	
+	@Override
+	public String toString()
+	{
+		return "FinalClass [name=" + name + "]";
+	}
+
+
+	public static void  main(String[] args)
+	{
+		List<FinalClass> list = new ArrayList<FinalClass>();
+		FinalClass f = new FinalClass("TL");
+		list.add(f);
+		
+		f.name ="afterChage";
+		
+		System.out.println(list);
+	}
+	
+	
+}
