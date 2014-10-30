683d501ba9303e3995e6a017f8bbaac25d67877a
TL-kaiya <917401186@qq.com>
2014-10-30 15:31:03 +0800

    rf

---
+++ src/aa/tangli/explore/TDebug.java
@@ -0,0 +1,21 @@
+/**
+ *	2014年10月27日 下午2:09:03
+ *	TangLi
+ */
+package aa.tangli.explore;
+
+/**
+ * @author TangLi
+ * 2014年10月27日下午2:09:03
+ */
+public class TDebug
+{
+	public static void main(String[] ars)
+	{
+		int i = 0 ;
+		i++;
+		i--;
+		i = 2;
+		System.out.println(i);
+	}
+}
---
+++ src/string/TString.java
@@ -0,0 +1,21 @@
+package string;
+
+
+public class TString
+{
+
+	public static void main(String[] ars)
+	{
+		String dateStr = "[\"2014/10/14\",\"2014/10/18\",\"2014/10/13\"]";
+		
+		System.out.println(dateStr);
+		
+		String[] list = ((String)dateStr.subSequence(1, dateStr.length()-1)).split(",");
+		
+		for(String s: list)
+		{
+			System.out.println(s);
+		}
+	}
+	
+}
