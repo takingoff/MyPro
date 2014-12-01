8da4696127faf546c7b8ccca9fc339e199219c8d
TL-kaiya <917401186@qq.com>
2014-12-01 14:17:12 +0800

    wew

--- src/design/model/singleton/ModelSingleton.java
+++ src/design/model/singleton/ModelSingleton.java
@@ -1,12 +1,31 @@
 package design.model.singleton;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class ModelSingleton
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		Singleton sn = Singleton.getInstance();
 		System.out.println(sn.resorce);
+		
+		Runtime rt = Runtime.getRuntime();
+		System.out.println(rt.maxMemory()/1024/1024);
+		System.out.println(rt.freeMemory()/1024/1024);
+		System.out.println(rt.totalMemory()/1024/1024);
+		
+//		rt.exit(0);
+		rt.gc();
+		Process p =  rt.exec("ping 127.0.0.1");
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(),"gbk"));
+		
+		char[] c = new char[20];;
+		do{System.out.print(c);}while(br.read(c)!=-1);
+		
 	}
 }
 
