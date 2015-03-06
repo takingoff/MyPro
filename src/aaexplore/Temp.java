4d6c3f69dba50b39e02174ee408d5dcbb6da8d53
TangLi <917401186@qq.com>
2015-03-06 10:04:01 +0800

    bit test

---
+++ src/aaexplore/Temp.java
@@ -0,0 +1,20 @@
+/**
+ *	2015年3月5日 上午9:23:54
+ *	TangLi
+ */
+package aaexplore;
+
+
+/**
+ * @author TangLi
+ * 2015年3月5日上午9:23:54
+ */
+public class Temp
+{
+	public static void main(String[] ar)
+	{
+		
+		System.out.println(Integer.toBinaryString(5));
+		
+	}
+}
