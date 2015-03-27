d33527020e8fa98788b785eae9a937f1e8b11ecd
TangLi <917401186@qq.com>
2015-03-27 17:01:37 +0800

    daf

--- src/aaexplore/Temp.java
+++ src/aaexplore/Temp.java
@@ -4,21 +4,64 @@
  */
 package aaexplore;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
- * @author TangLi
- * 2015年3月5日上午9:23:54
+ * @author TangLi 2015年3月5日上午9:23:54
  */
 public class Temp
 {
 	public static void main(String[] ar)
 	{
-		
+
 		char c = '2';
 		byte b = 2;
-		
-		int d = c+b;
+
+		int d = c + b;
 		System.out.println(d);
+
 		
+		System.out.println(getMD5Str("tangli"));
 	}
+
+	public static String getMD5Str(String str)
+	{
+		MessageDigest messageDigest = null;
+
+		try
+		{
+			messageDigest = MessageDigest.getInstance("MD5");
+
+			messageDigest.reset();
+
+			messageDigest.update(str.getBytes("UTF-8"));
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			System.out.println("NoSuchAlgorithmException caught!");
+			System.exit(-1);
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+		}
+
+		byte[] byteArray = messageDigest.digest();
+
+		StringBuffer md5StrBuff = new StringBuffer();
+
+		for (int i = 0; i < byteArray.length; i++)
+		{
+			int g = 0xFF & byteArray[i];
+			System.out.println(g);
+			if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)
+				md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+			else
+				md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+		}
+		return md5StrBuff.toString();
+	}
+
 }
