9c0145c59ff094621055cb816abefe7c9ba99292
tangli <tangli@tangliTakingoff>
2015-09-11 21:54:52 +0800

    bean util...

---
+++ src/aaaautil/Util.java
@@ -0,0 +1,29 @@
+/**
+ *	2015年9月11日 下午9:23:53
+ *	tangli
+ */
+package aaaautil;
+
+/**
+ * @author tangli
+ * 2015年9月11日下午9:23:53
+ */
+public class Util
+{
+	
+	public static interface DoSome
+	{
+		public void doSome()throws Exception;
+	}
+	
+	public static void timing(DoSome ds) throws Exception
+	{
+		
+		long time = System.currentTimeMillis();
+		System.out.println("//**************************begin");
+		ds.doSome();
+		System.out.println("//**************************end for" + (System.currentTimeMillis()-time)+"ms");
+	}
+	
+	
+}
---
+++ src/reflection/bean/AboutBean.java
@@ -0,0 +1,104 @@
+/**
+ *	2015年9月11日 下午8:28:00
+ *	tangli
+ */
+package reflection.bean;
+
+import java.util.Date;
+
+import net.sf.cglib.beans.BeanCopier;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+
+import aaaautil.Util;
+import thibernate.TStudent;
+
+/**
+ * @author tangli 2015年9月11日下午8:28:00
+ */
+
+public class AboutBean
+{
+
+	public static void main(String[] ar) throws Exception
+	{
+		TStudent s = new TStudent();
+
+		s.setAge(11);
+		s.setBirth(new Date());
+		s.setId("xxx");
+		s.setName("asdfasdfasdfa");
+
+		TStudent s1 = new TStudent();
+		TStudent s2 = new TStudent();
+		TStudent s3 = new TStudent();
+		TStudent s4 = new TStudent();
+
+		int count = 100000;
+
+		// 极大次数下优势明显，很稳定
+		Util.timing(new Util.DoSome()
+		{
+			@Override
+			public void doSome() throws Exception
+			{
+				BeanCopier copier = BeanCopier.create(s.getClass(), s.getClass(), false);
+				// good
+				for (int i = 0; i < count; i++)
+				{
+					copier.copy(s, s4, null);
+				}
+
+			}
+		});
+		
+		// 极大次数下优势明显，很稳定
+		Util.timing(new Util.DoSome()
+		{
+			@Override
+			public void doSome() throws Exception
+			{
+				// good
+				for (int i = 0; i < count; i++)
+				{
+					org.springframework.beans.BeanUtils.copyProperties(s,s3);
+				}
+				
+			}
+		});
+
+		// 次数越少 优势越明显，速度虽次数变大而变慢
+		Util.timing(new Util.DoSome()
+		{
+			@Override
+			public void doSome() throws Exception
+			{
+				// good
+				for (int i = 0; i < count; i++)
+					PropertyUtils.copyProperties(s2, s);
+			}
+		});
+
+		// 次数极小时速度很快。
+		Util.timing(new Util.DoSome()
+		{
+			@Override
+			public void doSome() throws Exception
+			{
+				// too slow
+				for (int i = 0; i < count; i++)
+					BeanUtils.copyProperties(s1, s);
+			}
+		});
+		
+		
+
+		System.out.println(s1.getName());
+		System.out.println(s2.getName());
+		System.out.println(s3.getName());
+		System.out.println(s4.getName());
+
+	}
+
+}
