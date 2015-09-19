190626f7060141b9e30f70520f2278b11a28cd53
tangli <tangli@tangliTakingoff>
2015-09-19 09:14:56 +0800

    bean compare

--- src/reflection/bean/BeanCompare.java
+++ src/reflection/bean/BeanCompare.java
@@ -4,17 +4,10 @@
  */
 package reflection.bean;
 
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.Date;
 
-import org.apache.commons.lang3.StringUtils;
-
-
+import thibernate.TStudent;
 
 /**
  * @author tangli 2015年9月11日下午11:29:55
@@ -22,65 +15,39 @@ import org.apache.commons.lang3.StringUtils;
 public class BeanCompare
 {
 
-	/**
-	 * 比较两个对象差异数据
-	 * 
-	 * @param beforeobj
-	 * @param afterobj
-	 * @param cols
-	 *            比较字段
-	 * @param different
-	 *            差异？
-	 * @return
-	 * @throws Exception
-	 */
-	public static List<Comparison> compareobj(Object beforeobj, Object afterobj, Map<String, String> cols, boolean different) throws Exception
+	public static boolean compare(Object self, Object other) throws Exception
 	{
-		List<Comparison> sames = new ArrayList<Comparison>();
-		List<Comparison> differents = new ArrayList<Comparison>();
-		if (beforeobj == null)
-			throw new Exception("beforeobj不能为空!");
-		if (afterobj == null)
-			throw new Exception("afterobj不能为空!");
-		if (!beforeobj.getClass().isAssignableFrom(afterobj.getClass()))
+		Class<? extends Object> clazz = self.getClass();
+
+		Field[] declaredFields = clazz.getDeclaredFields();
+		for (Field field : declaredFields)
 		{
-			throw new Exception("两个对象不相同，无法比较");
+			field.setAccessible(true);
+			Object val1 = field.get(self);
+			Object val2 = field.get(other);
+			if ( (val1==null && val2 == null) ||(val1!=null && val1.equals(val2)))
+				continue;
+			return false;
 		}
-		// 去除对象所有字段
-		PropertyDescriptor[] descriptors = Introspector.getBeanInfo(beforeobj.getClass()).getPropertyDescriptors();
-		Set<String> keys = cols.keySet();
-		for (String col : keys)
-		{
-			for (PropertyDescriptor descriptor : descriptors)
-			{
-				String proname = descriptor.getName();
-				if (StringUtils.isNotEmpty(proname) && proname.equalsIgnoreCase(col))
-				{
-					Comparison comparison = new Comparison();
-					Method readmethod = descriptor.getReadMethod();
-					comparison.setBefore(readmethod.invoke(beforeobj));
-					comparison.setAfter(readmethod.invoke(afterobj));
-					comparison.setFieldType(descriptor.getPropertyType());
-					comparison.setField(proname);
-					comparison.setFieldName(cols.get(col));
-					if (comparison.isdifferent())
-					{
-						sames.add(comparison);
-					}
-					else
-					{
-						differents.add(comparison);
-					}
-				}
-			}
-		}
-		if (different)
-		{
-			return differents;
-		}
-		else
-		{
-			return sames;
-		}
+		return true;
+
+	}
+
+	public static void main(String[] ars) throws Exception
+	{
+		TStudent s = new TStudent();
+		s.setAge(112);
+		s.setBirth(new Date());
+		s.setId("xxx");
+		s.setName("asdfasdfasdfa");
+
+		TStudent s1 = new TStudent();
+		s1.setAge(12);
+		s1.setBirth(new Date());
+		s1.setId("xxx");
+		s1.setName("asdfasdfasdfa");
+
+		System.err.println(compare(s, s1) ? "equal" : "not equal");
+
 	}
 }
