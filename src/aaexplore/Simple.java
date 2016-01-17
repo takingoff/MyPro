d67d9f98cf232fa8b110a3bc932bfdf9d003f5d0
tangli <tangli@tangliTakingoff>
2016-01-17 11:56:12 +0800

    fetch .....

--- src/aaexplore/Simple.java
+++ src/aaexplore/Simple.java
@@ -14,14 +14,6 @@ public class Simple
 	 * this is some "adff"
 	 */
 	public volatile static String StringFeild = "ss";
-	public static final String ffffff = "tttttttttttttttttttt";
-	@SuppressWarnings("unused")
-	private int age = 110;
-	protected long longField = 1000L;
-	double doublefield = 12.2;
-	boolean bool=true;
-	char charField = 'g';
-	short shortField = 20;
 	float floatField = 23.1f; 
 	
 	public void myMethod1()throws Exception
@@ -32,8 +24,11 @@ public class Simple
 	@Deprecated
 	public void myMethod2()
 	{
-		System.out.println("ss".split("fs").toString());
-		System.out.println(Simple.ffffff);
-		System.out.println(((int)'c'));
+		System.out.println(StringFeild);
+	}
+	
+	public static void main(String[] ar)
+	{
+		
 	}
 }
--- src/arithmetic/express/ScriptExecutor.java
+++ src/arithmetic/express/ScriptExecutor.java
@@ -17,14 +17,17 @@ import javax.script.ScriptException;
 public class ScriptExecutor
 {
 
+	/*SUN提供的JDK6中有一个命令行工具——jrunscript，你可以在<JDK6_Home>/bin下面找到这个工具，jrunscript是一个脚本语言的解释程序，
+	它独立于脚本语言，但默认是用JavaScript，我们可以用jrunscript来测试自己写的脚本*/
+	
 	public static void main(String[] args) throws Exception
 	{
 		ScriptEngineManager manager = new ScriptEngineManager();
 		ScriptEngine engine = manager.getEngineByName("JavaScript");
-//		testScriptVariables(engine);// 演示如何暴露Java对象为脚本语言的全局变量
-//		testInvokeScriptMethod(engine);// 演示如何在Java中调用脚本语言的方法
-//		testScriptInterface(engine);// 演示脚本语言如何实现Java的接口
-		testUsingJDKClasses(engine);// 演示脚本语言如何使用JDK平台下的类
+		testScriptVariables(engine);// 演示如何暴露Java对象为脚本语言的全局变量
+		testInvokeScriptMethod(engine);// 演示如何在Java中调用脚本语言的方法
+		testScriptInterface(engine);// 演示脚本语言如何实现Java的接口
+//		testUsingJDKClasses(engine);// 演示脚本语言如何使用JDK平台下的类
 	}
 
 	public static void testScriptVariables(ScriptEngine engine) throws Exception
